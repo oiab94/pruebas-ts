767ec343a8cc3c003436d704b4111afa87b4106e
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-05-12 16:00:28 -0400

    Las interfaces permiten crear nuestro tipo de dato

---
+++ tipos-objetos/src/interfaces.ts
@@ -0,0 +1,33 @@
+interface Person {
+	name: string,
+	age: number
+}
+
+/**
+ * Controlamos los datos que recibimos con el if
+ * @param person puede ser de tipo person, null o undefined
+ * @returns no retorna ningun dato por ende void 
+ */
+function hello(person: Person | null | undefined): void {
+	if(!person){
+		console.log("Ingrese un nombre");
+		return;
+	}
+
+	console.log(`${person.name, person.age}`);
+}
+
+/**
+ * No controlamos la función
+ * @param person Recibe un dato del tipo person
+ * @returns retornamos el saludo completo
+ */
+function greet(person: Person): string {
+	return `Hello ${person.name}`;
+}
+
+const person_1 = {name: "oscar", age: 28};
+let nombre = undefined;
+hello(nombre);
+console.log(greet(person_1));
+nombre = "asdf";
